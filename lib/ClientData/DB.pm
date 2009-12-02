ac07e8d666b58f1853d09a2e7b3dfcf6ed2d66e6
Ivan Nevostruev <halyava@gmail.com>
2009-12-02 15:52:06 +0000

    RESOLVED - issue SSP-19274: Puntillo, Anthony - ampuntillo - can we produce patients and email addresses that are on the unsubscribe list for this office? http://hippo.3treepoint.com:8080/browse/SSP-19274
    git-svn-id: svn://localhost/trunk/sesame/utils@1504 468d5dd6-d73f-d64d-b57d-226c4fa5b953

--- lib/ClientData/DB.pm
+++ lib/ClientData/DB.pm
@@ -8,17 +8,17 @@ use DBI;
 use Sesame::Unified::Client;
 
 sub new {
-    my ($class, $data_source, $db_name) = @_;
+	my ($class, $data_source, $db_name) = @_;
 
-    my $client_ref = Sesame::Unified::Client->new('db_name', $db_name);
-    my $client_type = $client_ref->get_client_type();
+	my $client_ref = Sesame::Unified::Client->new('db_name', $db_name);
+	my $client_type = $client_ref->get_client_type();
 
-    my $dbi = $data_source->get_connection( $db_name );
-    if ($client_type eq 'dental') {
-    	$class = 'ClientData::DB::Dental';
+	my $dbi = $data_source->get_connection( $db_name );
+	if ($client_type eq 'dental') {
+		$class = 'ClientData::DB::Dental';
 		require ClientData::DB::Dental;
-    }
-    else {
+	}
+	else {
 		my $context = $dbi->selectrow_array(<<'SQL', undef ,$db_name);
 SELECT s.dbs_context
 FROM sesameweb.clients cl
@@ -26,26 +26,26 @@ LEFT JOIN sesameweb.udbf_software s ON (s.dbs_id=cl.cl_pms)
 WHERE cl.cl_mysql=?
 SQL
 		if ($context eq 'pat') {
-	    	$class = 'ClientData::DB::OrthoPat';
+			$class = 'ClientData::DB::OrthoPat';
 			require ClientData::DB::OrthoPat;
 		}
 		elsif ($context eq 'resp') {
-	    	$class = 'ClientData::DB::OrthoResp';
+			$class = 'ClientData::DB::OrthoResp';
 			require ClientData::DB::OrthoResp;
 		}
 		else {
 			die "unknown Ortho context [$context] for [$db_name]";
 		}
-    }
+	}
 
-    my $self = bless {
-        'dbh' => $dbi,
-        'data_source' => $data_source,
-        'strict_search' => 1,
-        'db_name' => $db_name,
-    }, $class;
+	my $self = bless {
+		'dbh' => $dbi,
+		'data_source' => $data_source,
+		'strict_search' => 1,
+		'db_name' => $db_name,
+	}, $class;
 
-    return $self;
+	return $self;
 }
 
 sub get_db_name {
@@ -61,81 +61,90 @@ sub set_strict_level {
 }
 
 sub _search_by_name {
-    my ($self, $fields, $table, $fname, $lname, $where) = @_;
+	my ($self, $fields, $table, $fname, $lname, $where) = @_;
 
 	if (defined $where) {
 		$where = " AND $where";
 	}
-    my $result;
-    if (defined $lname) {
-    	$result = $self->{'dbh'}->selectall_arrayref(
-	        "SELECT $fields FROM $table WHERE FName=? AND LName=?$where",
-	        { 'Slice' => {} },
-	        $fname, $lname
-	    );
-    }
-    else {
-    	$result = $self->{'dbh'}->selectall_arrayref(
-	        "SELECT $fields FROM $table WHERE CONCAT(FName, ' ', LName)=?$where",
-	        { 'Slice' => {} },
-	        $fname
-	    );
-    }
-    unless ($self->{'strict_search'}) {
-	    unless (@$result) {
-		    if (defined $lname) {
-	    	    $result = $self->{'dbh'}->selectall_arrayref(
-	        	    "SELECT $fields FROM $table WHERE CONCAT(FName, ' ', LName) LIKE ?$where",
-	            	{ 'Slice' => {} },
-	            	$self->_string_to_like("$fname $lname"),
-		        );
-		    }
-		    else {
-	    	    $result = $self->{'dbh'}->selectall_arrayref(
-	        	    "SELECT $fields FROM $table WHERE ? LIKE CONCAT('%', FName, ' ', LName,'%')$where",
-	            	{ 'Slice' => {} },
-	            	$fname,
-		        );
-		    }
-	    }
-    }
-    return $result;
+	my $result;
+	if (defined $lname) {
+		$result = $self->{'dbh'}->selectall_arrayref(
+			"SELECT $fields FROM $table WHERE FName=? AND LName=?$where",
+			{ 'Slice' => {} },
+			$fname, $lname
+		);
+	}
+	else {
+		$result = $self->{'dbh'}->selectall_arrayref(
+			"SELECT $fields FROM $table WHERE CONCAT(FName, ' ', LName)=?$where",
+			{ 'Slice' => {} },
+			$fname
+		);
+	}
+	unless ($self->{'strict_search'}) {
+		unless (@$result) {
+			if (defined $lname) {
+				$result = $self->{'dbh'}->selectall_arrayref(
+					"SELECT $fields FROM $table WHERE CONCAT(FName, ' ', LName) LIKE ?$where",
+					{ 'Slice' => {} },
+					$self->_string_to_like("$fname $lname"),
+				);
+			}
+			else {
+				$result = $self->{'dbh'}->selectall_arrayref(
+					"SELECT $fields FROM $table WHERE ? LIKE CONCAT('%', FName, ' ', LName,'%')$where",
+					{ 'Slice' => {} },
+					$fname,
+				);
+			}
+		}
+	}
+	return $result;
 }
 
 sub _string_to_like {
-    my ($self, $str) = @_;
+	my ($self, $str) = @_;
 
-    $str =~ s/\b/ /g;
-    $str =~ s/\s+/%/g;
-    return $str;
+	$str =~ s/\b/ /g;
+	$str =~ s/\s+/%/g;
+	return $str;
 }
 
 sub add_colleague {
-    my ($self, $fname, $lname, $email, $password) = @_;
+	my ($self, $fname, $lname, $email, $password) = @_;
 
-    my $new_id = 1 + $self->{'dbh'}->selectrow_array("SELECT max(id) FROM referring_contacts");
+	my $new_id = 1 + $self->{'dbh'}->selectrow_array("SELECT max(id) FROM referring_contacts");
 
-    my $insert_cmd = "INSERT INTO referring_contacts (id, fname, lname, practice_name, email, speciality) VALUES (".$self->{'dbh'}->quote($new_id).", ".$self->{'dbh'}->quote($fname).", ".$self->{'dbh'}->quote($lname).", NULL, ".$self->{'dbh'}->quote($email).", NULL)";
-    $self->{'dbh'}->do($insert_cmd);
+	my $insert_cmd = "INSERT INTO referring_contacts (id, fname, lname, practice_name, email, speciality) VALUES (".$self->{'dbh'}->quote($new_id).", ".$self->{'dbh'}->quote($fname).", ".$self->{'dbh'}->quote($lname).", NULL, ".$self->{'dbh'}->quote($email).", NULL)";
+	$self->{'dbh'}->do($insert_cmd);
 	$self->{'data_source'}->add_statement($insert_cmd);
 
-    my $si_insert_cmd = "INSERT INTO SI_Doctor (FName, LName, Status, Password, Deleted, WelcomeSent, PrivacyAccepted, ref_contact_id, AutoNotify) VALUES (NULL, NULL, 1, ".$self->{'dbh'}->quote($password).", 0, 0, 0, ".$self->{'dbh'}->quote($new_id).", 0)";
-    $self->{'dbh'}->do($si_insert_cmd);
+	my $si_insert_cmd = "INSERT INTO SI_Doctor (FName, LName, Status, Password, Deleted, WelcomeSent, PrivacyAccepted, ref_contact_id, AutoNotify) VALUES (NULL, NULL, 1, ".$self->{'dbh'}->quote($password).", 0, 0, 0, ".$self->{'dbh'}->quote($new_id).", 0)";
+	$self->{'dbh'}->do($si_insert_cmd);
 	$self->{'data_source'}->add_statement($si_insert_cmd);
 
-    my $ref_insert_cmd = "INSERT INTO referrings (ref_fname, ref_lname, ref_email, ref_contact_id) VALUES (".$self->{'dbh'}->quote($fname).", ".$self->{'dbh'}->quote($lname).", ".$self->{'dbh'}->quote($email).", ".$self->{'dbh'}->quote($new_id).")";
-    $self->{'dbh'}->do($ref_insert_cmd);
+	my $ref_insert_cmd = "INSERT INTO referrings (ref_fname, ref_lname, ref_email, ref_contact_id) VALUES (".$self->{'dbh'}->quote($fname).", ".$self->{'dbh'}->quote($lname).", ".$self->{'dbh'}->quote($email).", ".$self->{'dbh'}->quote($new_id).")";
+	$self->{'dbh'}->do($ref_insert_cmd);
 	$self->{'data_source'}->add_statement($ref_insert_cmd);
 }
 
 sub email_exists_by_colleague {
-    my ($self, $email) = @_;
+	my ($self, $email) = @_;
 
-    return scalar $self->{'dbh'}->selectrow_array(
-        "SELECT count(*) FROM referring_contacts WHERE email=?",
-        undef,
-        $email,
-    );
+	return scalar $self->{'dbh'}->selectrow_array(
+		"SELECT count(*) FROM referring_contacts WHERE email=?",
+		undef,
+		$email,
+	);
+}
+
+sub get_unsubscribed_emails {
+	my ($self) = @_;
+
+	return $self->{'dbh'}->selectall_arrayref(
+		"SELECT Email, Type FROM unsubscribe",
+		{ 'Slice' => {} },
+	);
 }
 
 
--- lib/ClientData/DB/Ortho.pm
+++ lib/ClientData/DB/Ortho.pm
@@ -5,7 +5,7 @@ use base qw( ClientData::DB );
 
 sub get_reverse_remap {
 	my ($self, $table_id, $id) = @_;
-	
+
 	return $self->{'dbh'}->selectrow_array(
 		"SELECT orig_id FROM remap WHERE id=? AND table_id=?",
 		undef,
@@ -16,7 +16,7 @@ sub get_reverse_remap {
 
 sub add_remap {
 	my ($self, $table_id, $id, $orig_id) = @_;
-	
+
 	$self->{'dbh'}->do(
 		"INSERT INTO remap (table_id, id, orig_id) VALUES (?, ?, ?)",
 		undef,
@@ -28,7 +28,7 @@ sub add_remap {
 
 sub get_all_remap_tables {
 	my ($self) = @_;
-	
+
 	return $self->{'dbh'}->selectall_arrayref(
 		"SELECT id, name FROM sesameweb.remap_tables",
 		{ 'Slice' => {} },
@@ -37,7 +37,7 @@ sub get_all_remap_tables {
 
 sub get_all_ids_by_table_name {
 	my ($self, $table_name) = @_;
-	
+
 	my %known_ids = (
 		'procedures' => {
 			'table' => 'procedures',
@@ -64,6 +64,26 @@ sub get_emails_by_responsible {
 	);
 }
 
+sub get_emails_by_address {
+	my ($self, $email) = @_;
+
+	return $self->{'dbh'}->selectall_arrayref(
+		$self->_get_emails_select()." WHERE ml_email=?",
+		{ 'Slice' => {} },
+		$email,
+	);
+}
+
+sub get_moved_emails_by_address {
+	my ($self, $email) = @_;
+
+	return $self->{'dbh'}->selectall_arrayref(
+		$self->_get_emails_select(1)." WHERE ml_email=?",
+		{ 'Slice' => {} },
+		$email,
+	);
+}
+
 sub get_emails_by_pid {
 	my ($self, $pid) = @_;
 
@@ -76,34 +96,49 @@ sub get_emails_by_pid {
 
 sub get_all_emails {
 	my ($self) = @_;
-	
+
 	return $self->{'dbh'}->selectall_arrayref(
 		$self->_get_emails_select(),
 		{ 'Slice' => {} },
 	);
 }
 
-sub _get_emails_select {
+sub get_all_moved_emails {
 	my ($self) = @_;
 
-	return <<'SQL',
-SELECT 
-	ml_id AS id, 
-	ml_resp_id AS RId, 
-	ml_pat_id AS PId, 
-	ml_belongsto AS BelongsTo, 
-	ml_email AS Email, 
+	return $self->{'dbh'}->selectall_arrayref(
+		$self->_get_emails_select(1),
+		{ 'Slice' => {} },
+	);
+}
+
+sub _get_emails_select {
+	my ($self, $is_moved) = @_;
+
+	my $moved_fields = ( $is_moved ? <<SQL : '');
+	ml_moved_date AS MovedDate,
+	ml_moved_source AS MovedSource,
+SQL
+	my $table = ( $is_moved ? 'moved_mails' : 'maillist');
+	return <<SQL;
+SELECT
+$moved_fields
+	ml_id AS id,
+	ml_resp_id AS RId,
+	ml_pat_id AS PId,
+	ml_belongsto AS BelongsTo,
+	ml_email AS Email,
 	ml_name AS Name,
-    ml_date AS Date, 
-	ml_status AS Status,   
+    ml_date AS Date,
+	ml_status AS Status,
 	ml_source AS Source
-FROM maillist 
+FROM $table
 SQL
 }
 
 sub get_responsibles {
 	my ($self) = @_;
-	
+
 	return $self->{'dbh'}->selectall_arrayref(
 		"SELECT RId, FName, LName FROM responsibles",
 		{ 'Slice' => {} },
@@ -112,7 +147,7 @@ sub get_responsibles {
 
 sub get_responsible_by_id {
 	my ($self, $rid) = @_;
-	
+
 	return $self->{'dbh'}->selectrow_hashref(
 		"SELECT RId, FName, LName FROM responsibles WHERE RId=?",
 		undef,
@@ -158,9 +193,9 @@ sub get_patients_by_name {
     my ($self, $fname, $lname) = @_;
 
     return $self->_search_by_name(
-    	'PId, FName, LName, BDate, Phone, Status', 
-    	'patients', 
-    	$fname, 
+    	'PId, FName, LName, BDate, Phone, Status',
+    	'patients',
+    	$fname,
     	$lname,
     );
 }
@@ -169,10 +204,10 @@ sub get_patients_by_name_and_ids {
     my ($self, $fname, $lname, $ids) = @_;
 
     return $self->_search_by_name(
-    	'PId, FName, LName, BDate, Phone, Status', 
-    	'patients', 
-    	$fname, 
-    	$lname, 
+    	'PId, FName, LName, BDate, Phone, Status',
+    	'patients',
+    	$fname,
+    	$lname,
     	"PId IN (".join(', ', map {$self->{'dbh'}->quote($_)} @$ids).")",
     );
 }
@@ -181,9 +216,9 @@ sub get_patients_by_name_and_phone_and_ids {
     my ($self, $fname, $lname, $phone, $ids) = @_;
 
     return $self->_search_by_name(
-    	'PId, FName, LName, BDate, Phone, Status', 
-    	'patients', 
-    	$fname, 
+    	'PId, FName, LName, BDate, Phone, Status',
+    	'patients',
+    	$fname,
     	$lname,
     	"Phone LIKE ".$self->{'dbh'}->quote($self->_string_to_like($phone)).
     	"AND PId IN (".join(', ', map {$self->{'dbh'}->quote($_)} @$ids).")",
@@ -198,7 +233,7 @@ sub get_responsible_ids_by_name {
 
 sub get_patient_by_id {
 	my ($self, $pid) = @_;
-	
+
 	return $self->{'dbh'}->selectrow_hashref(
 		"SELECT PId, FName, LName, BDate, Phone, Status FROM patients WHERE PId=?",
 		undef,
@@ -227,16 +262,16 @@ sub get_addresses_by_pid {
 
 sub get_visited_offices {
 	my ($self) = @_;
-	
+
     return $self->{'dbh'}->selectall_arrayref(
         "SELECT PId, OfficeId, count(*) AS Count FROM ah_app_history GROUP BY 1, 2",
 		{ 'Slice' => {} },
-    );	
+    );
 }
 
 sub get_offices {
 	my ($self) = @_;
-	
+
 	return $self->{'dbh'}->selectall_arrayref(
 		"SELECT OfficeId, OfficeName, OfficeLocation FROM offices",
 		{ 'Slice' => {} },
@@ -267,18 +302,18 @@ sub add_email {
     my ($self, $pid, $rid, $email, $belongs_to, $name, $status, $source) = @_;
 
 	my @params = map {$self->{'dbh'}->quote($_)} (
-		$rid,    $pid,   $belongs_to, 
-    	$email,  $name,  $status, 
+		$rid,    $pid,   $belongs_to,
+    	$email,  $name,  $status,
     	$source
     );
 	my $sql = sprintf(<<'SQL', @params);
-INSERT INTO maillist 
+INSERT INTO maillist
 	(ml_resp_id, ml_pat_id, ml_belongsto,
-	 ml_email, ml_name, ml_date, 
-	 ml_status,  ml_source) 
-VALUES 
-	(%s, %s, %s, 
-	%s, %s, NOW(), 
+	 ml_email, ml_name, ml_date,
+	 ml_status,  ml_source)
+VALUES
+	(%s, %s, %s,
+	%s, %s, NOW(),
 	%s, %s)
 SQL
 	$sql =~ s/\r?\n/ /g;
