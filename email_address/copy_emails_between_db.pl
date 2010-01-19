977781903bda20ef06bc4e7898663598b7a5b5c5
Ivan Nevostruev <halyava@gmail.com>
2010-01-19 19:15:59 +0000

    SSP-19708: Trapani, Andrew - atrapani - We need the South Elgin Emails http://hippo.3treepoint.com:8080/browse/SSP-19708
    git-svn-id: svn://localhost/trunk/sesame/utils@1659 468d5dd6-d73f-d64d-b57d-226c4fa5b953

--- email_address/copy_emails_between_db.pl
+++ email_address/copy_emails_between_db.pl
@@ -2,6 +2,7 @@
 ## $Id$
 
 use strict;
+use warnings;
 
 use lib qw( ../lib );
 
@@ -11,88 +12,91 @@ use DataSource::DB;
 my ($db_from, $db_to) = @ARGV;
 
 if (defined $db_to) {
-	printf("coping emails from [%s] to [%s]\n", $db_from, $db_to);
-	my $data_source = DataSource::DB->new();
-	my $client_data_from = $data_source->get_client_data_by_db($db_from);
-	my $client_data_to   = $data_source->get_client_data_by_db($db_to);
-	
+	printf("copying emails from [%s] to [%s]\n", $db_from, $db_to);
+	my $data_source_from = ( $db_from =~ s/^4:// ? DataSource::DB->new_4() : DataSource::DB->new() );
+	my $data_source_to   = DataSource::DB->new();
+	my $client_data_from = $data_source_from->get_client_data_by_db($db_from);
+	my $client_data_to   = $data_source_to->get_client_data_by_db($db_to);
+
 	my %candidate_selectors = (
 		'ortho_resp' => {
 			'ortho_resp' => \&select_candidate_resp_resp,
+			'sesame' => \&select_candidate_resp_sesame,
 		},
 		'ortho_pat' => {
 			'ortho_resp' => \&select_candidate_pat_resp,
 		},
 	);
-	my $candidate_selector = 
+	my %add_email = (
+		'ortho_resp' => \&add_email_with_responsible_party,
+		'sesame' => \&add_email_to_visitor,
+	);
+	my $candidate_selector =
 		$candidate_selectors{
-			$client_data_from->get_full_type() 
+			$client_data_from->get_full_type()
 		}{
-			$client_data_to->get_full_type() 
+			$client_data_to->get_full_type()
 		};
 	unless (defined $candidate_selector) {
 		die "can't convert data from [".$client_data_from->get_full_type()."] to [".$client_data_to->get_full_type()."]";
 	}
-	
+
+	my $start_time = time();
 	my $from_emails = $client_data_from->get_all_emails();
 	my $added_count = 0;
 	for my $from_email (@$from_emails) {
-		my $candidate_manager = CandidateManager->new(
-			{
-				'by_pat_resp_with_phone' => 1,
-				'by_pat_resp' => 2,
-				'by_resp' => 3,
-			}
-		);
-		
-		$candidate_selector->(
-			$from_email, 
-			$client_data_from, 
-			$client_data_to, 
-			$candidate_manager,
-		);
-
-		my $candidate = $candidate_manager->get_single_candidate();
-		if (defined $candidate) {
-			my $email_exists;
-			if ($client_data_to->get_full_type() eq 'ortho_resp') {
-				$email_exists = $client_data_to->email_exists_by_rid(
-					$from_email->{'Email'},
-					$candidate->{'responsible'}{'RId'},
-				);
-			}
-			else {
-				die "email exists not implemented for [".$client_data_to->get_full_type()."] type";
-			}
-			if ($email_exists) {
-				printf(
-					"SKIP: email [%s] is already added\n", 
-					$from_email->{'Email'}, 
-				);
-			}
-			else {
-				printf(
-					"ADD: %s\n", 
-					$from_email->{'Email'}, 
-				);
-				$client_data_to->add_email(
-					(defined $candidate->{'patient'} ? $candidate->{'patient'}{'PId'} : 0 ),
-					$candidate->{'responsible'}{'RId'},
-					$from_email->{'Email'},
-					$from_email->{'BelongsTo'},
-					$from_email->{'Name'},
-					$from_email->{'Status'},
-					$from_email->{'Source'},
-				);
-				$added_count++;
-			}
+		if ($client_data_to->email_is_used( $from_email->{'Email'} )) {
+			printf(
+				"SKIP: email [%s] is already used\n",
+				$from_email->{'Email'},
+			);
 		}
 		else {
-			printf(
-				"SKIP: %s - %s\n", 
-				$from_email->{'Email'}, 
-				$candidate_manager->candidates_count_str(),
+			my $candidate_manager = CandidateManager->new(
+				{
+					'by_pat_resp_with_phone' => 1,
+					'by_pat_resp' => 2,
+					'by_resp' => 3,
+					'by_pat' => 4,
+				}
 			);
+
+			$candidate_selector->(
+				$from_email,
+				$client_data_from,
+				$client_data_to,
+				$candidate_manager,
+			);
+
+			my $candidate = $candidate_manager->get_single_candidate();
+			if (defined $candidate) {
+				printf(
+					"ADD: %s\n",
+					$from_email->{'Email'},
+				);
+				$add_email{ $client_data_to->get_full_type() }->(
+					$client_data_to,
+					$from_email,
+					$candidate,
+				);
+#				$client_data_to->add_email(
+#					(defined $candidate->{'patient'} ? $candidate->{'patient'}{'PId'} : 0 ),
+#					$candidate->{'responsible'}{'RId'},
+#					$from_email->{'Email'},
+#					$from_email->{'BelongsTo'},
+#					$from_email->{'Name'},
+#					$from_email->{'Status'},
+#					$from_email->{'Source'},
+#				);
+				$added_count++;
+			}
+			else {
+				printf(
+					"SKIP: %s - %s\n",
+					$from_email->{'Email'},
+					$candidate_manager->candidates_count_str(),
+				);
+			}
 		}
 	}
 #    for my $sql (@{ $data_source->get_statements() }) {
@@ -100,33 +104,70 @@ if (defined $db_to) {
 #    }
 	my $result_sql_fn = '_new_email.'.$db_to.'.sql';
 	printf("write sql commands to [$result_sql_fn]\n");
-	$data_source->save_sql_commands_to_file($result_sql_fn);
-	printf("[%d] emails added\n", $added_count);
+	$data_source_to->save_sql_commands_to_file($result_sql_fn);
+	printf("[%d] of [%d] emails added\n", $added_count, scalar @$from_emails);
+	my $work_time = time() - $start_time;
+	printf "done in %d:%02d\n", $work_time / 60, $work_time % 60;
 }
 else {
 	print("Usage: $0 <db_from> <db_to>\n");
 	exit(1);
 }
 
-
-sub select_candidate_resp_resp {
+sub select_candidate_resp_sesame {
 	my ($from_email, $client_data_from, $client_data_to, $candidate_manager) = @_;
-	
+
 	if ($from_email->{'RId'}) {
 		my $from_responsible = $client_data_from->get_responsible_by_id(
 			$from_email->{'RId'},
 		);
-		my $to_responsibles = $client_data_to->get_responsible_ids_by_name( 
-			$from_responsible->{'FName'}, 
-			$from_responsible->{'LName'}, 
+		my $to_visitors = $client_data_to->get_responsibles_by_name(
+			$from_responsible->{'FName'},
+			$from_responsible->{'LName'},
+		);
+		for my $visitor (@$to_visitors) {
+			$candidate_manager->add_candidate(
+				'by_resp',
+				$visitor,
+			);
+		}
+		if ($from_email->{'PId'}) {
+			my $from_patient = $client_data_from->get_patient_by_id(
+				$from_email->{'PId'},
+			);
+			my $to_pat_visitors = $client_data_to->get_patients_by_name(
+				$from_patient->{'FName'},
+				$from_patient->{'LName'},
+			);
+			for my $visitor (@$to_pat_visitors) {
+				$candidate_manager->add_candidate(
+					'by_pat',
+					$visitor,
+				);
+			}
+		}
+	}
+
+}
+
+sub select_candidate_resp_resp {
+	my ($from_email, $client_data_from, $client_data_to, $candidate_manager) = @_;
+
+	if ($from_email->{'RId'}) {
+		my $from_responsible = $client_data_from->get_responsible_by_id(
+			$from_email->{'RId'},
+		);
+		my $to_responsibles = $client_data_to->get_responsible_ids_by_name(
+			$from_responsible->{'FName'},
+			$from_responsible->{'LName'},
 		);
 		for my $to_responsible (@$to_responsibles) {
 			if ($from_email->{'PId'}) {
 				select_candidate_for_patient(
-					$client_data_from, 
-					$client_data_to, 
-					$to_responsible, 
-					$from_email->{'PId'}, 
+					$client_data_from,
+					$client_data_to,
+					$to_responsible,
+					$from_email->{'PId'},
 					$candidate_manager,
 				);
 			}
@@ -145,15 +186,15 @@ sub select_candidate_resp_resp {
 
 sub select_candidate_for_patient {
 	my ($client_data_from, $client_data_to, $to_responsible, $from_pid, $candidate_manager) = @_;
-	
+
 	my $from_patient = $client_data_from->get_patient_by_id(
 		$from_pid,
 	);
 	my $to_patients_ids = $client_data_to->get_patient_ids_by_responsible(
 		$to_responsible->{'RId'},
 	);
-	my $to_patients = $client_data_to->get_patients_by_name_and_ids( 
-		$from_patient->{'FName'}, 
+	my $to_patients = $client_data_to->get_patients_by_name_and_ids(
+		$from_patient->{'FName'},
 		$from_patient->{'LName'},
 		$to_patients_ids,
 	);
@@ -167,8 +208,8 @@ sub select_candidate_for_patient {
 		);
 	}
 	if ($from_patient->{'Phone'}) {
-		my $to_patients_with_phone = $client_data_to->get_patients_by_name_and_phone_and_ids( 
-			$from_patient->{'FName'}, 
+		my $to_patients_with_phone = $client_data_to->get_patients_by_name_and_phone_and_ids(
+			$from_patient->{'FName'},
 			$from_patient->{'LName'},
 			$from_patient->{'Phone'},
 			$to_patients_ids,
@@ -187,7 +228,7 @@ sub select_candidate_for_patient {
 
 sub select_candidate_pat_resp {
 	my ($from_email, $client_data_from, $client_data_to, $candidate_manager) = @_;
-	
+
 	if ($from_email->{'PId'}) {
 		my $from_patient = $client_data_from->get_patient_by_id($from_email->{'PId'});
 		my $from_responsibles;
@@ -197,20 +238,20 @@ sub select_candidate_pat_resp {
 		else {
 			$from_responsibles = $client_data_from->get_responsible_ids_by_patient($from_email->{'PId'});
 		}
-		
+
 		for my $from_responsible_id (@$from_responsibles) {
 			my $from_responsible = $client_data_from->get_responsible_by_id($from_responsible_id);
 
-			my $to_responsibles = $client_data_to->get_responsible_ids_by_name( 
-				$from_responsible->{'FName'}, 
-				$from_responsible->{'LName'}, 
+			my $to_responsibles = $client_data_to->get_responsible_ids_by_name(
+				$from_responsible->{'FName'},
+				$from_responsible->{'LName'},
 			);
 			for my $to_responsible (@$to_responsibles) {
 				select_candidate_for_patient(
-					$client_data_from, 
-					$client_data_to, 
-					$to_responsible, 
-					$from_email->{'PId'}, 
+					$client_data_from,
+					$client_data_to,
+					$to_responsible,
+					$from_email->{'PId'},
 					$candidate_manager,
 				);
 			}
@@ -218,4 +259,58 @@ sub select_candidate_pat_resp {
 	}
 }
 
+sub add_email_with_responsible_party {
+	my ($client_data, $email, $candidate) = @_;
+
+	$client_data->add_email(
+		(defined $candidate->{'patient'} ? $candidate->{'patient'}{'PId'} : 0 ),
+		$candidate->{'responsible'}{'RId'},
+		$email->{'Email'},
+		$email->{'BelongsTo'},
+		$email->{'Name'},
+		$email->{'Status'},
+		$email->{'Source'},
+	);
+
+}
+
+sub add_email_to_visitor {
+	my ($client_data, $email, $candidate) = @_;
+
+	my %source_map = (
+		'sesame'     => 'sesame',
+		'staff'      => 'staff',
+		'cp'         => 'cp',
+		'web_emc'    => 'online_collector',
+		'office_emc' => 'office_collector',
+		'doctor_db'  => 'pms_old',
+		'other'      => 'other',
+		'online_collector' => 'online_collector',
+		'office_collector' => 'office_collector',
+		'pms_old'    => 'pms_old',
+		'pms_new'    => 'pms_new',
+	);
+	my %resp_type = (
+		'0' => '',
+		'1' => 'family',
+		'2' => 'mother',
+		'3' => 'father',
+		'4' => 'other',
+		''       => '',
+		'family' => 'family',
+		'mother' => 'mother',
+		'father' => 'father',
+		'other'  => 'other',
+	);
+
+	my $visitor = $candidate;
+	$client_data->add_email(
+		$visitor->{'id'},
+		$email->{'Email'},
+		( exists $resp_type{ $email->{'BelongsTo'} } ? $resp_type{ $email->{'BelongsTo'} } : 'other' ),
+		$email->{'Name'},
+		( $email->{'Status'} ? 'true' : 'false' ),
+		( exists $source_map{ $email->{'Source'} } ? $source_map{ $email->{'Source'} } : 'other' ),
+	);
+}
 
