d706d7bc0a0b6e757148dfb17000be3012db23b7
Daniel Carleton <dacc@puttyjar.com>
2011-10-03 13:52:14 -0700

    startup logging.  removed redundant reading of bonecp system properties.

--- jetty-console-jndi-datasource-plugin/src/main/java/com/sesamecom/util/JndiDataSourcePlugin.java
+++ jetty-console-jndi-datasource-plugin/src/main/java/com/sesamecom/util/JndiDataSourcePlugin.java
@@ -5,18 +5,19 @@ import org.eclipse.jetty.webapp.WebAppContext;
 import org.simplericity.jettyconsole.api.DefaultStartOption;
 import org.simplericity.jettyconsole.api.JettyConsolePluginBase;
 import org.simplericity.jettyconsole.api.StartOption;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
-import static java.lang.Integer.parseInt;
-import static java.lang.Long.parseLong;
-
 /**
  * A Jetty Console plugin that creates a JNDI data source using persist and bonecp system properties.
  */
 public class JndiDataSourcePlugin extends JettyConsolePluginBase {
+    private static final Logger log = LoggerFactory.getLogger(JndiDataSourcePlugin.class);
+
     private BoneCPDataSource dataSource;
 
     private StartOption startOption = new DefaultStartOption(
@@ -43,6 +44,8 @@ public class JndiDataSourcePlugin extends JettyConsolePluginBase {
 
         String url = String.format("jdbc:mysql://%s:%s/%s?characterEncoding=utf8", host, port, schema);
 
+        log.info("Bootstrapping JNDI DataSource @ /comp/env/jdbc/SesameDB with {}", url);
+
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -53,26 +56,6 @@ public class JndiDataSourcePlugin extends JettyConsolePluginBase {
         dataSource.setJdbcUrl(url);
         dataSource.setUsername(user);
         dataSource.setPassword(pass);
-
-        String idleMaxAgeInMinutes = System.getProperty("bonecp.idleMaxAgeInMinutes", "240");
-        String idleConnectionTestPeriodInMinutes = System.getProperty("bonecp.idleConnectionTestPeriodInMinutes", "60");
-        String connectionTestStatement = System.getProperty("bonecp.connectionTestStatement", "/* ping */ SELECT 1");
-        String partitionCount = System.getProperty("bonecp.partitionCount", "3");
-        String acquireIncrement = System.getProperty("bonecp.acquireIncrement", "10");
-        String maxConnectionsPerPartition = System.getProperty("bonecp.maxConnectionsPerPartition", "50");
-        String minConnectionsPerPartition = System.getProperty("bonecp.minConnectionsPerPartition", "10");
-        String statementsCacheSize = System.getProperty("bonecp.statementsCacheSize", "50");
-        String releaseHelperThreads = System.getProperty("bonecp.releaseHelperThreads", "3");
-
-        dataSource.setIdleMaxAgeInMinutes(parseLong(idleMaxAgeInMinutes));
-        dataSource.setIdleConnectionTestPeriodInMinutes(parseLong(idleConnectionTestPeriodInMinutes));
-        dataSource.setConnectionTestStatement(connectionTestStatement);
-        dataSource.setPartitionCount(parseInt(partitionCount));
-        dataSource.setAcquireIncrement(parseInt(acquireIncrement));
-        dataSource.setMaxConnectionsPerPartition(parseInt(maxConnectionsPerPartition));
-        dataSource.setMinConnectionsPerPartition(parseInt(minConnectionsPerPartition));
-        dataSource.setStatementsCacheSize(parseInt(statementsCacheSize));
-        dataSource.setReleaseHelperThreads(parseInt(releaseHelperThreads));
 
         try {
             InitialContext ic = new InitialContext();
