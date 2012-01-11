a0b14604b5e6e693832d0c691f20a99fad8b39f8
Bill Landers <blanders@sesamecommunications.com>
2012-01-11 10:31:47 -0800

    https://jira.sesamecommunications.com:8443/browse/SPD-9

--- cdyne-ws-client/src/main/java/com/sesamecom/cdyne/soap/client/SmsNotifyServiceAxis2Impl.java
+++ cdyne-ws-client/src/main/java/com/sesamecom/cdyne/soap/client/SmsNotifyServiceAxis2Impl.java
@@ -1,9 +1,6 @@
 package com.sesamecom.cdyne.soap.client;
 
-import com.cdyne.sms2.SMSRequest;
-import com.cdyne.sms2.SMSRequestDocument;
-import com.cdyne.sms2.SimpleSMSsendDocument;
-import com.cdyne.sms2.SimpleSMSsendWithPostbackDocument;
+import com.cdyne.sms2.*;
 import com.cdyne.sms2.impl.SMSRequestImpl;
 import com.microsoft.schemas._2003._10.serialization.arrays.impl.ArrayOfstringImpl;
 import com.sesamecom.soap.generated.cdyne.SmsStub;
@@ -12,19 +9,46 @@ import org.datacontract.schemas._2004._07.smsws.SMSAdvancedRequestDocument;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.rmi.RemoteException;
+
 public class SmsNotifyServiceAxis2Impl implements SmsNotifyService {
     private static final Logger log = LoggerFactory.getLogger(SmsNotifyService.class);
+    
+    private static final String SMS_LICENSE_KEY = "97050845-77d1-450b-9c14-3f813b86754c";
 
     @Override
-    public long simpleSmsSendWithPostback(String phoneNumber, String licenseKey, String message, String postbackUrl) {
-        long queueId = -1;
+    public String simpleSmsSendWithPostback(String phoneNumber, String licenseKey, String message, String postbackUrl) {
+        String messageId = "";
+
         try {
             SmsStub smsStub = new SmsStub();
+            
+            SimpleSMSsendWithPostbackDocument sspd = SimpleSMSsendWithPostbackDocument.Factory.newInstance();
+            SimpleSMSsendWithPostbackDocument.SimpleSMSsendWithPostback simpleSMSsendWithPostback = 
+                    sspd.addNewSimpleSMSsendWithPostback();
+            
+            simpleSMSsendWithPostback.setPhoneNumber(phoneNumber);
+            simpleSMSsendWithPostback.setLicenseKey(SMS_LICENSE_KEY);
+            simpleSMSsendWithPostback.setMessage(message);
+            simpleSMSsendWithPostback.setStatusPostBackURL(postbackUrl);
+
+            try {
+                SimpleSMSsendWithPostbackResponseDocument responseDocument =
+                        smsStub.SimpleSMSsendWithPostback(sspd);
+
+                SimpleSMSsendWithPostbackResponseDocument.SimpleSMSsendWithPostbackResponse simpleSMSsendWithPostbackResponse =
+                        responseDocument.getSimpleSMSsendWithPostbackResponse();
+
+                SMSResponse response = simpleSMSsendWithPostbackResponse.getSimpleSMSsendWithPostbackResult();
+                messageId = response.getMessageID();
+            } catch (RemoteException e) {
+                log.error("Unable to get CDYNE SMS response", e);
+            }
 
         } catch (AxisFault e) {
             log.warn("error caught as axisFault", e);
         }
-        return queueId;
+        return messageId;
     }
 
     @Override
