f3c0d17b7aed2389be24caea4eb53683732cc66a
Ivan Nevostruev <halyava@gmail.com>
2012-03-28 17:36:54 -0700

    SPD-190 - MD > Holiday Card Preview uses incorrect size for flash

--- support-utils/templates/_parse_holiday_template.pl
+++ support-utils/templates/_parse_holiday_template.pl
@@ -1,7 +1,9 @@
+#!/usr/bin/perl
 use strict;
 use warnings;
 
 use Template;
+use Image::Magick;
 
 my ($fn) = @ARGV;
 if (defined $fn) {
@@ -9,7 +11,9 @@ if (defined $fn) {
 	my ($preview_url) = ($preview =~ m{src="([^"]+)"}i);
 	my $image = render_template($fn, {'size' => 'full'});
 	my ($image_url, $width, $height);
-	if ($image =~ m{OBJECT\s+classid}i) {
+	if ($image =~ m{SWFObject\("(.*)",\s*".*",\s*"(.*)",\s*"(.*)",}i) {
+		($image_url, $width, $height) = ($1, $2, $3);
+	} elsif ($image =~ m{OBJECT\s+classid}i) {
 		my ($embed) = ($image =~ m{<EMBED\s+(.*?)>}is);
 		($image_url) = ($embed =~ m{src="([^"]+)"}i);
 		($width) = ($embed =~ m{width="([^"]+)"}i);
@@ -19,6 +23,9 @@ if (defined $fn) {
 		($image_url) = ($image =~ m{src="([^"]+)"}i);
 		($width) = ($image =~ m{width="([^"]+)"}i);
 		($height) = ($image =~ m{height="([^"]+)"}i);
+	}
+	if (!defined $width && $image_url =~ m{\.gif$}i) {
+		($width, $height) = read_gif_size($image_url);
 	}
 	printf "%s,%s,%s,%s,%s\n", $fn, $image_url, $preview_url, $width, $height;
 }
@@ -34,4 +41,12 @@ sub render_template {
 	my $template = Template->new({'ABSOLUTE' => 1, 'RELATIVE' => 1});
 	$template->process($fn, $params, \$body) or die $template->error();
 	return $body;
+}
+
+sub read_gif_size {
+	my ($fn) = @_;
+
+	my $img = Image::Magick->new();
+	$img->Read($ENV{SESAME_COMMON}.$fn);
+	return $img->Get('width', 'height');
 }
