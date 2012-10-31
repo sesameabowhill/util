2b5a43bb3182b9accdd2dd9886a2a158ed45873c
Ivan Nevostruev <halyava@gmail.com>
2012-10-30 23:29:37 -0700

    Service status: adding message parsing

--- service-status/servicestatus.py
+++ service-status/servicestatus.py
@@ -1,29 +1,80 @@
 #!/usr/bin/env python
 
+import Queue
+import re
 import subprocess
 import threading
-import Queue
+import datetime
 
-class Record:
-	def __init__(self, line, fn):
+## TODO
+##  - version
+##  - configuration
+##  - service name
+
+class LogRecord:
+	def __init__(self, line, fn, date, status):
 		self.line = line
 		self.file = fn
+		self.date = date
+		self.status = status
+
+	NONE = "NONE"
+	STARTED = "STARTED"
+	WRAPPER_STARTED = "WRAPPER_STARTED"
+	WRAPPER_STOPPED = "WRAPPER_STOPPED"
+	WAITING_LIQUIBASE = "WAITING_LIQUIBASE"
+#STATUS | wrapper  | 2012/08/01 03:12:37 | <-- Wrapper Stopped
+#STATUS | wrapper  | 2012/08/01 03:12:38 | --> Wrapper Started as Daemon
+#INFO   | jvm 1    | 2012/09/04 23:09:59 | 23:09:59.356 [WrapperSimpleAppMain:DB=] INFO  o.e.jetty.server.AbstractConnector - Started @0.0.0.0:82
+#INFO   | jvm 1    | 2012/08/01 03:01:37 | Waiting for changelog lock....
+
 
 class QueueProcessor(threading.Thread):
-	def __init__(self, queue, exit_after):
+	def __init__(self, queue, exit_after, processor):
 		threading.Thread.__init__(self)
 		self.queue = queue
 		self.exit_after = exit_after
+		self.processor = processor
 	def run(self):
 		while True:
 			record = self.queue.get()
 			if record == None:
 				self.exit_after -= 1
 				if self.exit_after == 0:
+					self.processor.finish()
+					self.queue.task_done()
 					break
 			else:
-				print "==>>[%s]<<==" % record.line
-				self.queue.task_done()
+				self.processor.process(record)
+			self.queue.task_done()
+
+class LogRecordProcessor:		
+	def process(self, record):
+		pass
+	def finish(self):
+		pass
+
+class PrintLastStatus(LogRecordProcessor):
+	def __init__(self):
+		self.last = {}
+		self.last_control = {}
+	def process(self, record):
+		if record.status != LogRecord.NONE:
+			self.last_control[record.file] = record
+		if record.date != None:
+			self.last[record.file] = record
+	def finish(self):
+		for fn in sorted(iter(self.last)):
+			last = self.last[fn]
+			control = self.last_control[fn]
+			print "%s: %s - [%s] (waiting from %s)" % (fn, last.date, control.status, control.date)
+
+
+
+class PrintAllSystemMessages(LogRecordProcessor):
+	def process(self, record):
+		if record.status != LogRecord.NONE:
+			print "%s [%s]: %s" % (record.date, record.status, record.line)
 
 class LineReader:
 	def __init__(self, fn):
@@ -43,16 +94,39 @@ class ReaderThread(threading.Thread):
 		self.file = fn
 	def run(self):
 		for line in self.reader.lines():
-			self.queue.put(Record(line, self.file))
-			#print "==>>[%s]<<==" % line
+			matched_date = ReaderThread.DATE_RE.search(line)
+			if matched_date:
+				date = datetime.datetime(*[ int(i) for i in matched_date.groups() ])
+			else:
+				date = None
+
+			if ReaderThread.STARTED_RE.search(line):
+				status = LogRecord.STARTED
+			elif ReaderThread.WRAPPER_STARTED_RE.search(line):
+				status = LogRecord.WRAPPER_STARTED
+			elif ReaderThread.WRAPPER_STOPPED_RE.search(line):
+				status = LogRecord.WRAPPER_STOPPED
+			elif ReaderThread.WAITING_LIQUIBASE_RE.search(line):
+				status = LogRecord.WAITING_LIQUIBASE
+			else:
+				status = LogRecord.NONE
+
+			self.queue.put(LogRecord(line, self.file, date, status))
 		self.queue.put(None)
+	DATE_RE = re.compile('(\d+)[/-](\d+)[/-](\d+)\s+(\d+):(\d+):(\d+)')
+	STARTED_RE = re.compile('Started @\d+\.\d+\.\d+\.\d+:\d+')
+	WRAPPER_STARTED_RE = re.compile('^STATUS.*Wrapper Started')
+	WRAPPER_STOPPED_RE = re.compile('^STATUS.*Wrapper Stopped')
+	WAITING_LIQUIBASE_RE = re.compile('Waiting for changelog lock\.\.\.\.')
+
 
 
 class ServerStatus:
 	def start(self):
-		files = ("a.txt", "b.txt", "c.txt", "d.txt", "e.txt")
+		files = ["/Users/ivan/target/logs2/md.vpc-prd-web-01.log"]
 		queue = Queue.Queue();
-		processor = QueueProcessor(queue, len(files))
+		#processor = QueueProcessor(queue, len(files), PrintAllSystemMessages())
+		processor = QueueProcessor(queue, len(files), PrintLastStatus())
 		processor.start()
 		threads = [ ReaderThread(fn, queue) for fn in files ]
 		for thread in threads:
