7224552c81fdfb9096223b9890af58d23561b6de
Ivan Nevostruev <halyava@gmail.com>
2012-11-06 00:22:39 -0800

    Adding command line options

--- service-status/servicestatus.py
+++ service-status/servicestatus.py
@@ -5,6 +5,10 @@ import re
 import subprocess
 import threading
 import datetime
+import optparse
+import sys
+#import argparse
+
 
 ## TODO
 ##  - version
@@ -27,6 +31,18 @@ class LogRecord:
 #STATUS | wrapper  | 2012/08/01 03:12:38 | --> Wrapper Started as Daemon
 #INFO   | jvm 1    | 2012/09/04 23:09:59 | 23:09:59.356 [WrapperSimpleAppMain:DB=] INFO  o.e.jetty.server.AbstractConnector - Started @0.0.0.0:82
 #INFO   | jvm 1    | 2012/08/01 03:01:37 | Waiting for changelog lock....
+
+
+#INFO   | jvm 1    | 2012/11/02 00:15:32 | 00:15:32.838 [WrapperSimpleAppMain:DB=] INFO  c.s.util.ArtifactInfoReporter - artifact->buildInformation buildTag: jenkins-web-do-release-62, buildId: 2012-11-01_18-33-43, buildUrl: https://jenkins.sesamecom.com/job/web-do-release/62/, gitBranch: release, gitCommit: 45c8ef110e113c24b01c4e5125d0ce9c94c51ff9, applicationName: patient-pages-1.2.92, applicationVersion: 1.2.92
+#INFO   | jvm 1    | 2012/10/24 19:18:05 | 19:18:05.752 [WrapperSimpleAppMain] WARN  c.s.util.ArtifactInfoReporter - can't report version: /META-INF/MANIFEST.MF is not found in classpath
+#INFO   | jvm 1    | 2012/10/19 23:38:22 | 23:38:22.741 [WrapperSimpleAppMain:DB=] INFO  o.e.jetty.webapp.WebInfConfiguration - Extract jar:file:/home/sites/artifacts/md-test-20121019.222117-32-jetty-console.war!/ to /var/tmp/jetty-0.0.0.0-82-md-test-20121019.222117-32-jetty-console.war-_-any-/webapp
+
+
+
+#INFO   | jvm 1    | 2012/10/24 19:19:55 | 19:19:55.474 [WrapperSimpleAppMain] INFO  c.sesamecom.config.EnvironmentConfig - property->resolved name: janitorCleanupAlarmName, value: 'va-iet-web-01_cleanupAlarm', source: FILE, configFilePath: /home/sites/wrapper/conf/janitor/sesame.properties
+#INFO   | jvm 1    | 2012/10/24 19:18:00 | 19:18:00.324 [WrapperSimpleAppMain] WARN  c.sesamecom.config.EnvironmentConfig - optionalProperty->defaultValueUsed name: useBasicDataSource, defaultValue: false, configFilePath: /home/sites/wrapper/conf/janitor/sesame.properties
+#INFO   | jvm 1    | 2012/10/19 23:38:53 | 23:38:53.323 [WrapperSimpleAppMain:DB=] WARN  c.sesamecom.config.EnvironmentConfig - optionalProperty->defaultValueUsed name: useBasicDataSource, defaultValue: false, configFilePath: [none defined via -DsesameConfigurationFile]
+#INFO   | jvm 1    | 2012/10/19 23:38:55 | 23:38:55.163 [WrapperSimpleAppMain:DB=] INFO  c.sesamecom.config.EnvironmentConfig - property->resolved name: persistSchema, value: 'sesame_stage_db', source: SYSTEM, configFilePath: [none defined via -DsesameConfigurationFile]
 
 
 class QueueProcessor(threading.Thread):
@@ -54,22 +70,53 @@ class LogRecordProcessor:
 	def finish(self):
 		pass
 
+class GatherMetaData(LogRecordProcessor):
+	def __init__(self, file_name):
+		self.file_name = file_name
+		self.version = None
+		self.properties = {}
+	def process(self, record):
+		if record.status == LogRecord.NONE:
+			matched_version = GatherMetaData.VERSION_RE.search(record.line)
+			if matched_version:
+				matched_number = GatherMetaData.VERSION_NAME_RE.search(matched_version.group('version'))
+				if matched_number:
+					self.version = (matched_number.group('name'), matched_number.group('version'))
+				else:
+					self.version = (matched_version.group('version'), None);
+		elif record.status == LogRecord.WRAPPER_STARTED:
+			self.version = None
+			self.properties = {}
+	def finish(self):
+		print "%s: %s - [%s] (waiting from %s)" % (self.file_name, self.last.date, self.control.status, self.control.date)
+	VERSION_RE = re.compile('ArtifactInfoReporter\b.*\bapplicationName:\s+(?P<version>\S+)')
+	VERSION_NAME_RE = re.compile('^(?P<name>.*)-(?P<number>\d+\.\d+\.\d+|\w+-\d+\.\d+-\d+),?$')
+
+
 class PrintLastStatus(LogRecordProcessor):
-	def __init__(self):
-		self.last = {}
-		self.last_control = {}
+	def __init__(self, file_name):
+		self.file_name = file_name
+		self.last = None
+		self.control = None
 	def process(self, record):
 		if record.status != LogRecord.NONE:
-			self.last_control[record.file] = record
+			self.control = record
 		if record.date != None:
-			self.last[record.file] = record
+			self.last = record
 	def finish(self):
-		for fn in sorted(iter(self.last)):
-			last = self.last[fn]
-			control = self.last_control[fn]
-			print "%s: %s - [%s] (waiting from %s)" % (fn, last.date, control.status, control.date)
+		print "%s: %s - [%s] (waiting from %s)" % (self.file_name, self.last.date, self.control.status, self.control.date)
 
-
+class PerFileProcessor(LogRecordProcessor):
+	def __init__(self, line_processor):
+		self.per_file = {}
+		self.line_processor = line_processor
+	def process(self, record):
+		if record.file not in self.per_file:
+			self.per_file[record.file] = self.line_processor(record.file)
+		self.per_file[record.file].process(record)
+	def finish(self):
+		for file_name in sorted(iter(self.per_file)):
+			self.per_file[file_name].finish()
 
 class PrintAllSystemMessages(LogRecordProcessor):
 	def process(self, record):
@@ -122,11 +169,13 @@ class ReaderThread(threading.Thread):
 
 
 class ServerStatus:
-	def start(self):
-		files = ["/Users/ivan/target/logs2/md.vpc-prd-web-01.log"]
+	def start(self, files):
+		#files = ["/Users/ivan/target/logs2/md.vpc-prd-web-01.log", "/Users/ivan/target/logs2/uploadws.vpc-prd-ups-01.log"]
+		#files = ["/Users/ivan/target/logs2/started.log", "/Users/ivan/target/logs2/started2.log", \
+		#"/Users/ivan/target/logs2/waiting.log", "/Users/ivan/target/logs2/wrapper_started.log"]
 		queue = Queue.Queue();
 		#processor = QueueProcessor(queue, len(files), PrintAllSystemMessages())
-		processor = QueueProcessor(queue, len(files), PrintLastStatus())
+		processor = QueueProcessor(queue, len(files), PerFileProcessor(PrintLastStatus))
 		processor.start()
 		threads = [ ReaderThread(fn, queue) for fn in files ]
 		for thread in threads:
@@ -139,5 +188,15 @@ class ServerStatus:
 
 
 if __name__ == "__main__":
+	# parser = argparse.ArgumentParser(description='Watch log files.')
+	# parser.add_argument('files', type=str, nargs='+', help='Log file to watch')
+	# args = parser.parse_args()
+	parser = optparse.OptionParser()
+	parser.add_option("-f", "--file", dest="files", help="Log file to watch")
+	(options, args) = parser.parse_args()
+	if len(args) == 0:
+		print "need at least one file name"
+		sys.exit(1)
+
 	server_status = ServerStatus()
-	server_status.start()
+	server_status.start(args)
