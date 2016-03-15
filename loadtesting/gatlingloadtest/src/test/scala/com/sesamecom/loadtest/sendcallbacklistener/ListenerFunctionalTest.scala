9d5c97fa42c80ddd198a2a197a7a0ce63df95038
Jesse Mleziva <jmleziva@sesamecommunications.com>
2016-03-14 17:41:52 -0700

    Gatling Load Tests: Modified the load test to only contain good requests. Modified the functional test to take into account NGINX behavior.

--- loadtesting/gatlingloadtest/src/test/scala/com/sesamecom/loadtest/sendcallbacklistener/ListenerFunctionalTest.scala
+++ loadtesting/gatlingloadtest/src/test/scala/com/sesamecom/loadtest/sendcallbacklistener/ListenerFunctionalTest.scala
@@ -11,15 +11,31 @@ class ListenerFunctionalTest extends Simulation {
   val targetHostBaseUrl = System.getProperty("targetBaseUrl")
   val httpProtocol = http.baseURL(targetHostBaseUrl)
 
+  val message =
+    "{" +
+        "\"body\": \"Gatling Load Test\"," +
+        "\"bodySize\": 17," +
+        "\"dateCreated\": \"2016-03-14T15:17:09.815-07:00\"," +
+        "\"deleted\": false," +
+        "\"deviceId\": 226665609," +
+        "\"finalDestination\": \"4445556666\"," +
+        "\"finalSource\": \"5554443333\"," +
+        "\"fingerprint\": \"44455566665554443333\"," +
+        "\"hasAttachment\": false," +
+        "\"id\": 20160314151709815," +
+        "\"messageTransport\": 5," +
+        "\"messageType\": \"MO\"," +
+        "\"read\": false," +
+        "\"statusCode\": 4," +
+        "\"visible\": true" +
+    "}"
+
+
   val scn = scenario("JettyAsyncLoadTest")
      .exec(
        http("ok_request")
-         .post("/zipwhip/receive?member=johnsmith")
-         .formParam("""{"payload":"stuff"}""", ""))
-     .exec(
-       http("bad_no_path_after_zipwhip_vendor")
-         .post("/zipwhip/")
-         .check(status.is(400)))
+         .post("/zipwhip/receive?member=depic")
+         .body(StringBody(message)))
      .exec(
        http("zipwhip_bad_url")
          .post("/zipwhip/bad-request")
@@ -27,16 +43,12 @@ class ListenerFunctionalTest extends Simulation {
      .exec(
        http("bad_vendor_url")
          .post("/bogus/receive?member=johnsmith")
-         .formParam("""{"value" :"stuff"}""", "")
+         .body(StringBody(message))
          .check(status.is(404)))
      .exec(
        http("bad_correct_path_no_parameters")
          .post("/zipwhip/receive")
          .check(status.is(400)))
-     .exec(
-       http("bad_get_instead_of_post")
-         .put("/zipwhip/receive?member=johnsmith")
-         .check(status.is(405)))
 
    setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))
  }
--- loadtesting/gatlingloadtest/src/test/scala/com/sesamecom/loadtest/sendcallbacklistener/ListenerStressTest.scala
+++ loadtesting/gatlingloadtest/src/test/scala/com/sesamecom/loadtest/sendcallbacklistener/ListenerStressTest.scala
@@ -10,49 +10,47 @@ class ListenerStressTest extends Simulation {
   val targetHostBaseUrl = System.getProperty("targetBaseUrl")
   val httpProtocol = http.baseURL(targetHostBaseUrl)
 
+  val message =
+    "{" +
+        "\"body\": \"Gatling Load Test\"," +
+        "\"bodySize\": 17," +
+        "\"dateCreated\": \"2016-03-14T15:17:09.815-07:00\"," +
+        "\"deleted\": false," +
+        "\"deviceId\": 226665609," +
+        "\"finalDestination\": \"4445556666\"," +
+        "\"finalSource\": \"5554443333\"," +
+        "\"fingerprint\": \"44455566665554443333\"," +
+        "\"hasAttachment\": false," +
+        "\"id\": 20160314151709815," +
+        "\"messageTransport\": 5," +
+        "\"messageType\": \"MO\"," +
+        "\"read\": false," +
+        "\"statusCode\": 4," +
+        "\"visible\": true" +
+    "}"
+
   val scn = scenario("JettyAsyncLoadTest")
     .exec(
       http("ok_request_0")
-        .post("/zipwhip/receive?member=johnsmith")
-        .formParam("""{"payload":"stuff"}""", ""))
+        .post("/zipwhip/receive?member=depic")
+        .body(StringBody(message)))
     .exec(
       http("ok_request_1")
-        .post("/zipwhip/receive?member=testAdaGonzales")
-        .formParam("""{"name"""", """"AdaGonzales"}"""))
+        .post("/zipwhip/receive?member=depic")
+        .body(StringBody(message)))
     .exec(
       http("ok_request_2")
-        .post("/zipwhip/receive?member=testAllisonPalmer")
-        .formParam("""{"name"""", """"AllisonPalmer"}"""))
+        .post("/zipwhip/receive?member=depic")
+        .body(StringBody(message)))
     .exec(
       http("ok_request_3")
-        .post("/zipwhip/receive?member=testAnaWatson")
-        .formParam("""{"name"""", """"AnaWatson"}"""))
+        .post("/zipwhip/receive?member=depic")
+        .body(StringBody(message)))
     .exec(
       http("ok_request_4")
-        .post("/zipwhip/receive?member=testAndrePatrick")
-        .formParam("""{"name"""", """"AndrePatrick"}"""))
-    .exec(
-      http("bad_no_path_after_zipwhip_vendor")
-        .post("/zipwhip/")
-        .check(status.is(400)))
-    .exec(
-      http("zipwhip_bad_url")
-        .post("/zipwhip/bad-request")
-        .check(status.is(400)))
-    .exec(
-      http("bad_vendor_url")
-        .post("/bogus/receive?member=johnsmith")
-        .formParam("""{"value" :"stuff"}""", "")
-        .check(status.is(404)))
-    .exec(
-      http("bad_correct_path_no_parameters")
-        .post("/zipwhip/receive")
-        .check(status.is(400)))
-    .exec(
-      http("bad_get_instead_of_post")
-        .put("/zipwhip/receive?member=johnsmith")
-        .check(status.is(405)))
+        .post("/zipwhip/receive?member=depic")
+        .body(StringBody(message)))
 
   //setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))
-  setUp(scn.inject(rampUsers(20000) over(20 seconds)).protocols(httpProtocol))
+  setUp(scn.inject(rampUsers(10000) over(20 seconds)).protocols(httpProtocol))
 }
