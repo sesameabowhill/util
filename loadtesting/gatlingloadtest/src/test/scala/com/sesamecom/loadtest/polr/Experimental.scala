c6574f45d46b77fc645b91b1e9e75c72be8ca27d
sesameabowhill <abowhill@sesamecommunications.com>
2018-05-04 18:20:11 -0700

    Some initial forays into gatling with Polr
    Experimental.scala is the best bet for url testing so far, it uses
    a custom feeder.
    
    LongTest.scala is a generic test run ripped from the Gatling docs
    it takes a long time to run.
    
    ShortTest.scala is something similar to the original PolrTest.scala.
    
    PolrTest.scala was removed, but can be restored by ripping the example
    on wikipedia.

---
+++ loadtesting/gatlingloadtest/src/test/scala/com/sesamecom/loadtest/polr/Experimental.scala
@@ -0,0 +1,72 @@
+package com.sesamecom.loadtest.polr
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+// https://groups.google.com/d/msg/gatling/Kts2srUj1O0/5q8aPKNLorgJ
+import java.util.concurrent.atomic.AtomicInteger
+import scala.concurrent.duration._
+import scala.util.Random
+
+/**
+  * Smoke screen test of stats service urls
+  */
+class ExperimentalTest extends Simulation {
+
+  // https://stackoverflow.com/a/1269279
+  implicit def toInt(in:Integer) = in.intValue()
+
+  val POLR_BASE_PROPERTY = "polrBaseUrl"
+  val POLR_ACCESS_TOKEN = "polrAccessToken"
+
+  val offset = new AtomicInteger()
+
+  // Setup Scenario target
+  val targetHostBaseUrl = System.getProperty(POLR_BASE_PROPERTY)
+
+  // Fetch access token
+  val polrAccessToken = System.getProperty(POLR_ACCESS_TOKEN)
+
+  // build our own feeder to generate urls
+  val feeder = 
+    Iterator.continually(
+      Map(
+        "urx" -> (
+          "https://sesamecom.com/" + Random.alphanumeric.take(20).mkString
+          )
+        ) 
+      )
+
+  // set base url without browser caching
+  val httpConf = http.baseURL(targetHostBaseUrl).disableCaching
+
+  // create a scenario where we ask to shorten a random url
+  val myscenario = 
+    scenario("ExperimentalTest")
+      .feed(feeder)
+      .pause(1)
+      .exec { session =>
+        println(session)
+        session
+        }
+      .pause(1)
+      .exec( http("ShortenUrl")
+        .get( session =>
+        // https://alvinalexander.com/scala/how-to-create-multiline-strings-heredoc-in-scala-cookbook
+        s"""/api/v2/action/shorten?key=$polrAccessToken
+        |&url=${session("urx").as[String]}"""
+        .stripMargin.replaceAll("\n", " ")
+        )
+      )
+     
+  // run scenario against target
+  setUp(
+    myscenario.inject(
+      nothingFor(4 seconds),
+      atOnceUsers(5),
+      rampUsers(5) over (15 seconds),
+      constantUsersPerSec(5) during (20 seconds),
+      )
+    .protocols(httpConf)
+  )
+
+}
--- loadtesting/gatlingloadtest/src/test/scala/com/sesamecom/loadtest/polr/ShortTest.scala
+++ loadtesting/gatlingloadtest/src/test/scala/com/sesamecom/loadtest/polr/ShortTest.scala
@@ -2,12 +2,17 @@ package com.sesamecom.loadtest.polr
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+// https://groups.google.com/d/msg/gatling/Kts2srUj1O0/5q8aPKNLorgJ
 import java.util.concurrent.atomic.AtomicInteger
+import scala.concurrent.duration._
 
 /**
   * Smoke screen test of stats service urls
   */
-class PolrTest extends Simulation {
+class ShortTest extends Simulation {
+
+  // https://stackoverflow.com/a/1269279
+  implicit def toInt(in:Integer) = in.intValue()
 
   val POLR_BASE_PROPERTY = "polrBaseUrl"
   val POLR_ACCESS_TOKEN = "polrAccessToken"
@@ -16,16 +21,15 @@ class PolrTest extends Simulation {
 
   // Setup Scenario target
   val targetHostBaseUrl = System.getProperty(POLR_BASE_PROPERTY)
-  val httpProtocol = http.baseURL(targetHostBaseUrl)
+  val httpProtocol = http.baseURL(targetHostBaseUrl).disableCaching
 
   // Fetch access token
   val polrAccessToken = System.getProperty(POLR_ACCESS_TOKEN)
 
-
   // Shorten link
   //  curl -i 'http://127.0.0.1:8555/api/v2/action/shorten?key=a4cd7be3c3fa9ffbccb67053d44ff9&url=http://www.oracle.com/technetwork/java/javase/10-relnotes-4108314.html&response_type=json'
   //  HTTP/1.1 200 OK
-  //  Server: nginx/1.8.0
+  //  Se rver: nginx/1.8.0
   //  Content-Type: application/json
   //  Transfer-Encoding: chunked
   //  Connection: keep-alive
@@ -36,18 +40,46 @@ class PolrTest extends Simulation {
   //  Set-Cookie: laravel_session=eyJpdiI6Im16SWl4YzJ2bjlTRE13MllLbXNncEE9PSIsInZhbHVlIjoiOThPaEVONGN0S0dvNkUrZGlvaFdpeEorM2M5NEdEZUtzNk9kemtZR2N0c2ZyV25ZTFFGb0piVE9yZzlScDE0Y3VBSFJTUHNKbHV5UmE2b3dJc1F6VEE9PSIsIm1hYyI6ImJkY2E5YWZmNWY3MDc2NGQzYzk2ODRiN2RlZWYwZWRlNTRlZGI2ZmM5ZGI0NTgzMzAzOGUzNzAzM2E5OTM4YjIifQ%3D%3D; expires=Fri, 20-Apr-2018 21:26:16 GMT; Max-Age=7200; path=/; HttpOnly
   //
   //  {"action":"shorten","result":"http:\/\/10.70.0.132:8555\/cu0ZZ"}
-  val scn = scenario("PolrTest")
-    .exec(
-      http("ShortenUrl")
-        .get(s"/api/v2/action/shorten?key=$polrAccessToken&url=${createTargetUrl(offset.getAndIncrement)}"))
+
+  val scn = 
+    scenario("ShortTest")
+    .exec(http("ShortenUrl")
+    .get(
+      
+      s"""/api/v2/action/shorten?key=$polrAccessToken
+        |&url=${createTargetUrl(offset.getAndIncrement)}"""
+        .stripMargin.replaceAll("\n", " ")))
+      
+    .exec { session =>
+      println(session)
+      // return the original session
+      session
+      }
+
+//  val scn1 = 
+//    scenario("ShortTest")
+//    .exec(http("ShortenUrlXXX")
+//    .get(s"/api/v2/action/shorten?key=$polrAccessToken&url=${incrementer(offset.getAndIncrement)}"))
 
 
   // run scenario against target
-  setUp(scn.inject(atOnceUsers(5)).protocols(httpProtocol))
+  setUp(
+    scn.inject(
+      nothingFor(4 seconds),
+      atOnceUsers(10),
+      rampUsers(10) over (5 seconds),
+      constantUsersPerSec(20) during (15 seconds),
+      constantUsersPerSec(20) during (15 seconds) randomized,
+      )
+    .protocols(httpProtocol)
+  )
 
+  val incrementer = (increment:Int) => s"https://sesamecom.com/${increment}"
 
   /**
     * @return an incremented URL. Currently goes against scala principles as it utilizes share stated.
     */
   def createTargetUrl(increment: Int) = s"https://sesamecom.com/${increment}"
+
+//  def createTargetUrl(increment: Int) = incrementer(increment.toInt)
 }
